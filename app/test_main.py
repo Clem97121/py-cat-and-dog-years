761a03dc282b1861fad29eef211d69e83d72f92b
Anton Gaievyi <anton.gaievyi@gmail.com>
2026-04-03 22:34:20 +0200

    remade py-cat-and-dog-years

--- app/test_main.py
+++ app/test_main.py
@@ -13,13 +13,17 @@ from app.main import get_human_age
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
+        (-1, -5, [0, 0]),  # Кейс с отрицательными числами (out-of-range)
     ],
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
 
-def test_get_human_age_returns_list() -> None:
-    result = get_human_age(20, 20)
-    assert isinstance(result, list), "The function should return a list"
-    assert len(result) == 2, "The list should contain exactly two elements"
+def test_should_raise_error_with_non_integer_arguments() -> None:
+    # Проверка на некорректные типы данных (строки вместо чисел)
+    with pytest.raises(TypeError):
+        get_human_age("20", 20)
+
+    with pytest.raises(TypeError):
+        get_human_age(20, [20])
